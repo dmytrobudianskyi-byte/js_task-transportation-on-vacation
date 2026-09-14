a13b4aca11adb1708f5ab75180ffd1c132c3c6b4
Dmytro Budianskyi <dmytro.budianskyi@gmail.com>
2026-09-14 14:26:04 +0200

    completed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,16 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const costPerDay = 40;
+  let totalRentalCost = costPerDay * days;
+
+  if (days >= 7) {
+    totalRentalCost -= 50;
+  } else if (days >= 3) {
+    totalRentalCost -= 20;
+  }
+
+  return totalRentalCost;
 }
 
 module.exports = calculateRentalCost;
